1c6a593e5b997e6d7b5f389923adffea359681d1
sv3tlana742 <lightrays.me@gmail.com>
2021-03-11 13:22:21 +0700

    Create project

---
+++ src/test/java/ru/geekbrains/AppTest.java
@@ -0,0 +1,22 @@
+package ru.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println(">> Start test!\n");
+        assertTrue( true );
+        System.out.printf("\n << End test!\n");
+    }
+}
